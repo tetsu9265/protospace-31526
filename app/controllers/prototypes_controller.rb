cd98aa212b728b53c415e6d20e56184cbb36ce93
tetsu9265 <1013toru@gmail.com>
2021-01-22 13:34:01 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,62 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_prototype, only: [:show, :edit]
+  before_action :move_other_user_to_index, only: :edit
+
+  def index
+    @prototypes = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render new_prototype_path
+    end
+  end
+
+  def show
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+  end
+
+  def update
+    prototype = Prototype.find(params[:id])
+    if prototype.update(prototype_params)
+      redirect_to prototype_path(prototype.id)
+    else
+      @prototype = prototype
+      render :edit
+    end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    if prototype.destroy
+      redirect_to root_path
+    end
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def move_other_user_to_index
+    unless current_user.id == @prototype.user.id
+      redirect_to action: :index
+    end
+  end
+end
